cbea92843422c5f26c1f07ae8b1d306cfa3627a0
LiuShenLan <liushenlan1997@gmail.com>
2020-07-20 11:00:14 +0800

    change show.py

--- tools/show.py
+++ tools/show.py
@@ -2,30 +2,40 @@ import argparse
 from mmdet.apis import init_detector, inference_detector
 import mmcv
 
-
+# 读取参数
 parser = argparse.ArgumentParser(description='show img')
 parser.add_argument('--cfg', default=None, help='config file path')
 parser.add_argument('--img', help='img path')
 parser.add_argument('--ckpt', default=None, help='checkpoint file path')
+parser.add_argument('--out', default=None, help='output img path')
+parser.add_argument('--save', default=True, help='save or show img')
 args = parser.parse_args()
 
 if args.cfg==None:
     config_file = 'configs/reppoints/reppoints_moment_r50_fpn_1x_citypersons.py'
 else:
     config_file=args.cfg
+
 if args.ckpt==None:
     checkpoint_file = 'work_dirs/reppoints_moment_r50_fpn_1x_coco_20200330-b73db8d1.pth'
 else:
     checkpoint_file=args.ckpt
 
+if args.out==None:
+    output_file = 'data/result.jpg'
+else:
+    output_file=args.out
 
-# build the model from a config file and a checkpoint file
+img = args.img  # or img = mmcv.imread(img), which will only load it once
+
+# 建立模型
 model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
-# test a single image and show the results
-img = args.img  # or img = mmcv.imread(img), which will only load it once
+# 测试一张图片并显示结果
 result = inference_detector(model, img)
-# visualize the results in a new window
-# model.show_result(img, result)
-# or save the visualization results to image files
-model.show_result(img, result, out_file='data/result.jpg')
+if args.save == True:
+    model.show_result(img, result, out_file=output_file)    # 保存可视化结果到文件
+else:
+    model.show_result(img, result)  # 在新窗口中可视化结果
+
+
